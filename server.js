5c53542cf59b3bbfebb50d91749ca1532dbb4674
Abhishek kumar <aasalex21c@gmail.com>
2024-05-06 11:37:35 +0530

    implemented update functionality

--- server.js
+++ server.js
@@ -50,10 +50,42 @@ server.get("/todos", (req, res)=>{
     })
 
 })
+// {
+//   "message": "1 TODOS found",
+//   "data": [
+//       {
+//           "id": "T9XqqfXYh43NYAJp",
+//           "name": "Have to attempt weekly test",
+//           "tags": [
+//               "Study"
+//           ],
+//           "due_date": "2024-05-06T15:38:13.274Z",
+//           "is_completed": false
+//       }
+//   ]
+// }
 
-// server.put("/todo/update", (req, res)=>{
-    
-// })
+server.put("/todo/update", (req, res)=>{
+  const { id } = req.query;
+  console.log(id);
+  const DATA_PATH = path.join("data", "todo.json");
+
+  const TODO_DATA = JSON.parse(fs.readFileSync(DATA_PATH, {encoding: "utf-8"}));
+
+  const UPDATED_DATA = TODO_DATA.map((elem)=>{
+    if(elem.id===id){
+      elem.is_completed = true;
+    }
+    return elem;
+  });
+  
+  console.log(UPDATED_DATA);
+  fs.writeFileSync(DATA_PATH, JSON.stringify(UPDATED_DATA, null, '\t'));
+
+  res.status(httpStatus.ACCEPTED).json({
+    message : "Task Marked as Completed !"
+})
+})
 
 server.delete("/todo/delete", (req, res)=>{    
     
@@ -74,6 +106,7 @@ server.delete("/todo/delete", (req, res)=>{
 })
 
 server.use("/*", (request, response)=>{
+    // console.log(request.query)
     response.status(httpStatus.NOT_FOUND).json({
         message : "API Endpoint is not handled"
     })
